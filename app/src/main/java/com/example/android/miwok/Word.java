67d996b7fb87519b6c9624513d7f9a0d1e9fbb54
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2018-02-06 17:24:49 +0100

    Added images for three categories, also some changes to Word and WordAdapter classes have been done

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -3,10 +3,19 @@ package com.example.android.miwok;
 public class Word {
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     public Word(String miwokTranslation, String defaultTranslation){
         this.mMiwokTranslation = miwokTranslation;
         this.mDefaultTranslation = defaultTranslation;
+    }
+
+    public Word(String miwokTranslation, String defaultTranslation, int imageResourceId) {
+        this.mMiwokTranslation = miwokTranslation;
+        this.mDefaultTranslation = defaultTranslation;
+        this.mImageResourceId = imageResourceId;
     }
 
     public String getmMiwokTranslation() {
@@ -15,5 +24,13 @@ public class Word {
 
     public String getmDefaultTranslation() {
         return mDefaultTranslation;
+    }
+
+    public int getmImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public boolean hasImage(){
+        return mImageResourceId != -1;
     }
 }
